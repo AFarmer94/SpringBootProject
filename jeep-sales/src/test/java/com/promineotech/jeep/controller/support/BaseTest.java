de3be5801200fd1b469882f0e34167dcc489b492
Andrew Farmer <thefarmer94@gmail.com>
2022-09-22 20:56:27 -0500

    pushing week 13

---
+++ jeep-sales/src/test/java/com/promineotech/jeep/controller/support/BaseTest.java
@@ -0,0 +1,28 @@
+package com.promineotech.jeep.controller.support;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+
+import com.promineotech.jeep.entity.Jeep;
+import com.promineotech.jeep.entity.JeepModel;
+
+import lombok.Getter;
+
+public class BaseTest {
+	
+	
+	
+//	JeepModel model = JeepModel.WRANGLER;
+//	String trim = "Sport";
+//	String uri = String.format("http://localhost:%d/jeeps?model=%s&trim%s", serverPort, model, trim);
+//	
+//	ResponseEntity<List<Jeep>> response = restTemplate.exchange(uri,  HttpMethod.Get, null, new ParameterizedTypeReference<>() {});
+	
+
+}
